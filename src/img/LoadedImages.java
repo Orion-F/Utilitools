c07be27491d5f07aa993590212cf3f7ea2bbbf06
Scientist96 <oforowycz@gmail.com>
2015-12-29 17:43:05 -0500

    Added new images; fixed LoadedImages
    New images are loaded in LoadedImages. They are planned to be used for
    the Program Profiler tool.

--- src/img/LoadedImages.java
+++ src/img/LoadedImages.java
@@ -1,6 +1,5 @@
 package img;
 
-import javafx.CustomStage;
 import javafx.scene.image.Image;
 
 /**
@@ -21,7 +20,7 @@ public class LoadedImages {
      * <br><br><b>Size:</b> 14x14
      */
     public static final Image MINIMIZE_ICON = new Image(
-            CustomStage.class.getResourceAsStream("minimizeIcon.png"));
+            LoadedImages.class.getResourceAsStream("minimizeIcon.png"));
     
     /**
      * Icon used for a close button in a title bar.
@@ -29,6 +28,78 @@ public class LoadedImages {
      * <br><br><b>Size:</b> 14x14
      */
     public static final Image CLOSE_ICON = new Image(
-            CustomStage.class.getResourceAsStream("closeIcon.png"));
-
+            LoadedImages.class.getResourceAsStream("closeIcon.png"));
+    
+    /**
+     * Icon used for representing an annotation
+     * 
+     * <br><br><b>Size:</b> 16x16
+     */
+    public static final Image ANNOTATION_ICON = new Image(
+            LoadedImages.class.getResourceAsStream("annotationIcon.png"));
+    
+    /**
+     * Icon used for representing a class
+     * 
+     * <br><br><b>Size:</b> 16x16
+     */
+    public static final Image CLASS_ICON = new Image(
+            LoadedImages.class.getResourceAsStream("classIcon.png"));
+    
+    /**
+     * Icon used for representing an empty package
+     * 
+     * <br><br><b>Size:</b> 16x16
+     */
+    public static final Image EMPTY_PACKAGE_ICON = new Image(
+            LoadedImages.class.getResourceAsStream("emptyPackageIcon.png"));
+    
+    /**
+     * Icon used for representing an enum
+     * 
+     * <br><br><b>Size:</b> 16x16
+     */
+    public static final Image ENUM_ICON = new Image(
+            LoadedImages.class.getResourceAsStream("enumIcon.png"));
+    
+    /**
+     * Icon used for representing an interface
+     * 
+     * <br><br><b>Size:</b> 16x16
+     */
+    public static final Image INTERFACE_ICON = new Image(
+            LoadedImages.class.getResourceAsStream("interfaceIcon.png"));
+    
+    /**
+     * Icon used for representing a package
+     * 
+     * <br><br><b>Size:</b> 16x16
+     */
+    public static final Image PACKAGE_ICON = new Image(
+            LoadedImages.class.getResourceAsStream("packageIcon.png"));
+    
+    /**
+     * Icon used for representing a private method
+     * 
+     * <br><br><b>Size:</b> 16x16
+     */
+    public static final Image PRIVATE_METHOD_ICON = new Image(
+            LoadedImages.class.getResourceAsStream("privateMethodIcon.png"));
+    
+    /**
+     * Icon used for representing a protected method
+     * 
+     * <br><br><b>Size:</b> 16x16
+     */
+    public static final Image PROTECTED_METHOD_ICON = new Image(
+            LoadedImages.class.getResourceAsStream("protectedMethodIcon.png"));
+    
+    /**
+     * Icon used for representing a public method
+     * 
+     * <br><br><b>Size:</b> 16x16
+     */
+    public static final Image PUBLIC_METHOD_ICON = new Image(
+            LoadedImages.class.getResourceAsStream("publicMethodIcon.png"));
+    
 }
